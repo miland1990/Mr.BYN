68fb9389b37d37fd7e984a1d9673ddd39569e722
Mihail Landyuk <m.landyuk.admitad@gmail.com>
2018-04-10 15:34:16 +0300

    great refactoring in OOP style

--- bot.py
+++ bot.py
@@ -12,146 +12,287 @@ bot = telebot.TeleBot(token)
 
 
 # TODO: сделать приложением, которое работает во взаимодействии как с телеграмом, так и имеет веб-морду
-# TODO: сделать добавление токена Telegram при запуске скрипта
+# TODO: документация в md формате адекватная
+# TODO: убедиться, что приложегие безопасно со стороны сторонних пользователей (авторизацию настроить)
+# TODO: завести тестового бота
 # TODO: добавить тестирование
 # TODO: миграция и создания БД файлика автоматом
-# TODO: сделать кнопки в клаве
 # TODO: логика бонусов
 # TODO: логика ведения баланса (ввод источников доходов с описанием)
 # TODO: парсинг чеков
-# TODO: метрики и стата по видам
+# TODO: стата по видам
 # TODO: клепание отчета excel
 # TODO: расход по стройке (нужно сразу конвертер валют делать)
 # TODO: задолженноть по стройке
-# TODO: уведомление о накопленной сумме
 # TODO: сравнительный анализ
 # TODO: построение графиков
-# TODO: возможность редактирования (при неудачном посте или выборе неверной категории)
-# TODO: попробовать использовать API ПриорБанка
-# TODO: рефакторинг оптимальности запросов
+# TODO: возможность редактирования при неверно выбранной категории либо удаление (спортно) записи
 # TODO: возможность просмотра статы за указанный период времени и сравнение с аналогичными тремя периодами
 # TODO: возможность переноса платежа на следующий месяц
+# TODO: проверку на дубликат за последние 10 инпутов простых сообщений по категории и цене с обработкой типа "уверены?"
 
-PRIOR_RE_STR = r'^Priorbank\. Karta (?:\d\*{3}\d{4}). (?P<datetime>.{17}). ' \
-               r'Oplata (?P<prise>\d*[.,]?\d*) (?P<currency>[\w+]{3}). (?P<place>.*). Dostupno:'
+PRIOR_RE_STR = r'Priorbank\. Karta (?:\d\*{3}\d{4}). (?P<datetime>.{17}). ' \
+               r'Oplata (?P<prise>\d*[.,]?\d*) (?P<currency>[\w+]{3}). (?P<place>[^\.]+).*'
 PRIOR_SMS_RE = re.compile(PRIOR_RE_STR)
 PRISE_RE = re.compile(r'^\d+?\.\d+?$')
 PRIOR_DATETIME_FORMAT = '%d-%m-%y %H:%M:%S'
 
 
-def generate_bot_text(db):
-    td = date.today().replace(day=1)
-    month_start = mktime(td.timetuple())
-    result = db.stat_by_total_month(month_start) or 0
-    return u'Cпасибо. Израсходовано за месяц: {} BYN'.format(round(result))
+class BaseMrBYN:
+    def __init__(self, db, bot, expense_choices=config.EXPENSES):
+        self.db = db
+        self.bot = bot
+        self.expense_choices = expense_choices
 
+    _note = None
+    _prise = None
+    _message_timestamp = None
+    _month_start_timestamp = None
+    _last_chat_message_id = None
+    _chat_id = None
+    _message = None
+    _currency = u'BYN'
 
-@bot.message_handler(regexp=PRIOR_RE_STR)
-def prior_msg(message):
-    db = SQLighter(config.database_name)
-    response = consume_prior(message, db)
-    for message_timestamp, user_note in response:
+    UI_MESSAGE_DUPLICATE = u'Платеж ({}) уже был засчитан.'
+    UI_MESSAGE_MONTH_THANK = u'Cпасибо. Израсходовано за месяц: {} BYN.'
+    UI_MESSAGE_MONTH_STAT = u'Израсходовано за месяц: {} BYN.'
+    UI_MESSAGE_INVALID = u'Введите корректно расход. Первым словом должна быть цена. Слова разделяются пробелами.'
+    UI_MESSAGE_CANCELED = u'Пользователь {} отменил ввод платежа.'
+    UI_MESSAGE_UNKNOWN_FORMAT = u'Внимание! Соообщение "{}" - не распознано. Введите расход вручную.'
+
+    @property
+    def note(self):
+        if not self._note:
+            prise, note = self.user_prise_and_note
+            self._note = note
+            self._prise = prise
+        return self._note
+
+    @property
+    def prise(self):
+        if not self._prise:
+            prise, note = self.user_prise_and_note
+            self._note = note
+            self._prise = prise
+        return self._prise
+
+    @property
+    def message_timestamp(self):
+        if not self._message_timestamp:
+            self._message_timestamp = self._message.date
+        return self._message_timestamp
+
+    @property
+    def month_start_timestamp(self):
+        if not self._month_start_timestamp:
+            td = date.today().replace(day=1)
+            self._month_start_timestamp = mktime(td.timetuple())
+        return self._month_start_timestamp
+
+    @property
+    def user_prise_and_note(self):
+        parts = self._message.text.split()
+        prise = parts[0].replace(',', '.')
+        note = ' '.join(parts[1:])
+        return prise, note
+
+    def callback_reply_markup(self, stat_message_id=u'', button_message_id=u''):
         keyboard = telebot.types.InlineKeyboardMarkup()
-        previous_expense = db.find_prior_expense(message_timestamp)
-        if not previous_expense or len(previous_expense) > 1:
-            for index, purch in enumerate(config.PURCHASES, start=1):
-                callback_button = telebot.types.InlineKeyboardButton(text=purch, callback_data=index)
-                keyboard.add(callback_button)
-            bot.send_message(message.chat.id, u'"{}"'.format(user_note), reply_markup=keyboard)
+        choice_buttons = list(self.expense_choices) + [u'отмена ввода']
+        for index, purch in enumerate(choice_buttons, start=1):
+            callback_data = str(index) + u'|' + str(self._message.message_id) + u'|' + str(stat_message_id) + u'|' + str(button_message_id)
+            callback_button = telebot.types.InlineKeyboardButton(text=purch, callback_data=callback_data)
+            keyboard.add(callback_button)
+        return keyboard
+
+    @property
+    def get_payment_title(self):
+            return u'{}: {}'.format(self.note, self.prise)
+
+    @property
+    def is_valid_prise(self):
+        if PRISE_RE.match(self.prise) is None:
+            return self.prise.isdigit()
+        return True
+
+    # setter for 'Message' telegram instanse
+    def _set_message(self, message, text=None):
+        self._message = message
+        if text:
+            self._message.text = text
+
+    #  отправка сообщений в чат и редактирование сообщений
+    def send_to_chat_with_choice_buttons(self, text, stat_message_id=u''):
+        self.bot.send_message(self._message.chat.id, text, reply_markup=self.callback_reply_markup(stat_message_id))
+
+    def send_to_chat(self, text):
+        self.bot.send_message(self._message.chat.id, text)
+
+    def edit_message_from_chat(self, edited_text, message_id):
+        self.bot.edit_message_text(
+            chat_id=self._message.chat.id,
+            message_id=message_id,
+            text=edited_text
+        )
+
+    #  запросы в БД
+    def record_expense_row(self, expense=None):
+        self.db.record_item(
+            user_id=self._message.from_user.id,
+            message_id=self._message.message_id,
+            message_timestamp=self.message_timestamp,
+            prise=self.prise,
+            note=self.note,
+            expense=expense
+        )
+
+    def update_expense(self, expense, text=None):
+        self.db.update_expense(self._message.message_id, expense, text)
+
+    def delete_message_from_chat_by_message_id(self, message_id):
+        self.db.delete_expense_by_timestamp(message_id)
+
+    def find_expense_by_note(self):
+        expense_raw = self.db.find_expense(self.note)
+        if expense_raw:
+            return expense_raw[0]
+
+    def find_expense_by_timestamp(self, timestamp):
+        expense = self.db.find_expense_by_timestamp(timestamp)
+        if expense:
+            expense = expense[0]
+        return expense
+
+    #  статистика
+    def get_current_month_stat_message(self, template=UI_MESSAGE_MONTH_THANK):
+        stats = self.db.stat_by_total_month(self.month_start_timestamp)
+        value = round(stats[0] if stats else 0, 2)
+        return template.format(value)
+
+    def get_current_stats(self, message):
+        self._set_message(message)
+        self.send_to_chat(self.get_current_month_stat_message(template=self.UI_MESSAGE_MONTH_STAT))
+
+    def reply_to_choice(self, call):
+        raise NotImplementedError
+
+    def reply_to_message(self, message):
+        raise NotImplementedError
+
+
+class SimpleMrBYN(BaseMrBYN):
+
+    def reply_to_message(self, telegram_message_instance):
+        self._set_message(telegram_message_instance)
+        if not (self.note and self.is_valid_prise):
+            self.send_to_chat(text=self.UI_MESSAGE_INVALID)
         else:
-            db.delete_expense_by_timestamp(message_timestamp)
-            bot.send_message(message.chat.id, u'Данный расход уже был учтен ранее')
+            self.record_expense_row()
+            if not self.find_expense_by_note():
+                self.send_to_chat_with_choice_buttons(text=self.get_payment_title)
+            else:
+                self.send_to_chat(text=self.get_current_month_stat_message())
+
+    def reply_to_choice(self, call):
+        self._set_message(call.message)
+        expense_index, message_id, stat_message_id, _ = call.data.split(u'|')
+        if expense_index == str(len(self.expense_choices) + 1):
+            self.db.delete_expense_by_message_id(message_id)
+            edited_text = self.UI_MESSAGE_CANCELED.format(call.from_user.first_name)
+        else:
+            self.db.update_expense(message_id, expense_index)
+            edited_text = self.get_current_month_stat_message(template=self.UI_MESSAGE_MONTH_STAT)
+        self.bot.delete_message(call.message.chat.id, call.message.message_id)
+        self.send_to_chat(edited_text)
+
+
+class PriorMrBYN(BaseMrBYN):
+
+    MESSAGE_DELIMETER = u'\n'
+
+    def get_messages(self, message):
+        return message.text.split(self.MESSAGE_DELIMETER)
+
+    def get_timestamp_from_string(self, datetime_string):
+        return int(mktime(datetime.strptime(datetime_string, PRIOR_DATETIME_FORMAT).timetuple()))
+
+    def reply_to_message(self, message_instance):
+        messages = self.get_messages(message_instance)
+        stat_message_id = str(message_instance.message_id + len(messages) + 1)
+        button_message_counter = message_instance.message_id
+        for message in messages:
+            prior_sms = PRIOR_SMS_RE.match(message)
+            if not prior_sms:
+                text = self.UI_MESSAGE_UNKNOWN_FORMAT.format(message)
+                self._set_message(message_instance, text=text)
+                button_message_counter += 1
+                self.send_to_chat(text)
+                continue
+            self._set_message(message_instance, text=message)
+            self._prise = prior_sms.group(u'prise')
+            self._note = prior_sms.group(u'place')
+            self._currency = prior_sms.group(u'currency')
+            self._message_timestamp = self.get_timestamp_from_string(prior_sms.group(u'datetime'))
+            if self.find_expense_by_timestamp(self._message_timestamp):
+                text = self.UI_MESSAGE_DUPLICATE.format(self.get_payment_title)
+                self._set_message(message_instance, text=text)
+                button_message_counter += 1
+                self.send_to_chat(text)
+            else:
+                prev_note_expense = self.find_expense_by_note()
+                self.record_expense_row(prev_note_expense)
+                if not prev_note_expense or prev_note_expense == u'BLR MOBILE BANK':
+                    button_message_counter += 1
+                    self.bot.send_message(
+                        self._message.chat.id,
+                        self.get_payment_title,
+                        reply_markup=self.callback_reply_markup(stat_message_id, button_message_counter)
+                    )
+        self.send_to_chat(self.get_current_month_stat_message(template=self.UI_MESSAGE_MONTH_STAT))
+            
+    def reply_to_choice(self, call):
+        self._set_message(call.message)
+        expense_index, message_id, stat_message_id, buttons_message_id = call.data.split(u'|')
+        text = call.message.text.split(u':')[0]
+        if expense_index == str(len(self.expense_choices) + 1):
+            self.db.delete_expense_by_message_id(message_id, text=text)
+            self.bot.delete_message(call.message.chat.id, stat_message_id)
+        else:
+            self.db.update_expense(message_id, expense_index, text=text)
+        # edited_text = self.get_current_month_stat_message(template=self.UI_MESSAGE_MONTH_STAT)
+        # self.edit_message_from_chat(edited_text, stat_message_id)
+        self.bot.delete_message(call.message.chat.id, buttons_message_id)
+
+
+@bot.message_handler(commands=[u'stat'])
+def current_stats(message):
+    mr_dollar = SimpleMrBYN(SQLighter(), bot)
+    mr_dollar.get_current_stats(message)
+
+
+@bot.message_handler(func=lambda message: u'Priorbank. Karta' in message.text)
+def prior_msg(message):
+    mr_dollar = PriorMrBYN(SQLighter(), bot)
+    mr_dollar.reply_to_message(message)
 
 
 @bot.message_handler(content_types=["text"])
-def any_msg(message):
-    db = SQLighter(config.database_name)
-    if not message.text.startswith('/'):
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        note, user_note = consume_message(message, db)
-        if not note:
-            bot.send_message(message.chat.id, u'Введите корректно расход: "цена комментарий"')
-        else:
-            previous_expense = db.find_expense(note)
-            if not previous_expense or not previous_expense[0][0] or len(previous_expense) > 1:
-                for index, purch in enumerate(config.PURCHASES, start=1):
-                    callback_button = telebot.types.InlineKeyboardButton(text=purch, callback_data=index)
-                    keyboard.add(callback_button)
-                bot.send_message(message.chat.id, u'"{}"'.format(user_note), reply_markup=keyboard)
-            else:
-                db.update_expense(message.message_id, previous_expense[0][0])
-                bot.send_message(message.chat.id, generate_bot_text(db))
+def simple_msg(message):
+    mr_dollar = SimpleMrBYN(SQLighter(), bot)
+    mr_dollar.reply_to_message(message)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.endswith(u'|'))
+def reply_to_simple_choice(call):
+    mr_dollar = SimpleMrBYN(SQLighter(), bot)
+    mr_dollar.reply_to_choice(call)
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    db = SQLighter(config.database_name)
-    if call.data == str(len(config.PURCHASES)):
-        db.delete_expense_by_id(call.message.message_id - 1)
-        if call.message:
-            bot.delete_message(
-                chat_id=call.message.chat.id,
-                message_id=call.message.message_id,
-            )
-            bot.send_message(call.message.chat.id, u'не засчитано из-за отмены ввода данных')
-    else:
-        db.update_expense(call.message.message_id - 1, call.data)
-        if call.message:
-            bot.edit_message_text(
-                chat_id=call.message.chat.id,
-                message_id=call.message.message_id,
-                text=generate_bot_text(db)
-            )
+def reply_to_prior_choice(call):
+    mr_dollar = PriorMrBYN(SQLighter(), bot)
+    mr_dollar.reply_to_choice(call)
 
-
-def is_number_regex(s):
-    if PRISE_RE.match(s) is None:
-        return s.isdigit()
-    return True
-
-
-def get_parts(line):
-    parts = line.split()
-    prise = parts[0].replace(',', '.').replace(u'ю', u'.')
-    note = ' '.join(parts[1:])
-    return prise, note
-
-
-def consume_prior(message, db):
-    purchase = message.text
-    messages = filter(None, purchase.split('\n'))
-    result = []
-    for purchase in messages:
-        prior_sms = PRIOR_SMS_RE.match(purchase)
-        message_timestamp = mktime(datetime.strptime(prior_sms.group('datetime'), PRIOR_DATETIME_FORMAT).timetuple())
-        raw_note = prior_sms.group('place')
-        user_note = u'{}: {}'.format(prior_sms.group('place'), prior_sms.group('prise'))
-        db.record_item(
-            message.from_user.id,
-            message.message_id,
-            message_timestamp,
-            prior_sms.group('prise'),
-            raw_note
-        )
-        result.append((message_timestamp, user_note),)
-    return result
-
-
-def consume_message(message, db):
-    purchase = message.text
-    message_prise, raw_note = get_parts(purchase)
-    user_note = u'{}: {}'.format(raw_note, message_prise)
-    if not is_number_regex(message_prise) or not raw_note:
-        return None, None
-    message_timestamp = message.date
-    db.record_item(
-        message.from_user.id,
-        message.message_id,
-        message_timestamp,
-        message_prise,
-        raw_note
-    )
-    return raw_note, user_note
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
--- database_api.py
+++ database_api.py
@@ -5,58 +5,93 @@ import config
 
 class SQLighter:
 
-    def __init__(self, database):
+    def __init__(self):
         self.connection = sqlite3.connect(config.database_name)
         self.cursor = self.connection.cursor()
 
     def set_up_tables(self):
         """ Начальная миграция """
         with self.connection:
-            self.cursor.execute('CREATE TABLE IF NOT EXISTS finance (id integer PRIMARY KEY,user_id text NOT NULL,message_id text NOT NULL,message_timestamp integer NOT NULL,expense text,prise text NOT NULL,note text NOT NULL);')
-            self.cursor.execute('CREATE TABLE IF NOT EXISTS expenses (id integer PRIMARY KEY, expense text NOT NULL);')
-            for expense in config.PURCHASES:
-                self.cursor.execute('INSERT INTO expenses (expense) VALUES (?);', (expense,))
+            self.connection.executescript(u"""
+                
+                CREATE TABLE IF NOT EXISTS finance (
+                id integer PRIMARY KEY,
+                user_id text NOT NULL,
+                message_id text NOT NULL,
+                message_timestamp integer NOT NULL,
+                expense text,
+                prise text NOT NULL,
+                note text NOT NULL
+                );
+                
+                CREATE TABLE IF NOT EXISTS expenses (
+                id integer PRIMARY KEY, 
+                expense text NOT NULL
+                );
+                
+                
+            """)
+            self.cursor.executemany('INSERT INTO expenses (expense) VALUES (?);', config.EXPENSES)
 
-    def record_item(self, user_id, message_id, message_timestamp, prise, note):
+    def record_item(self, user_id, message_id, message_timestamp, prise, note, expense):
         """ Запишем расход в БД """
         with self.connection:
             self.cursor.execute(
-                u"""INSERT INTO finance 
-                    (user_id, message_id, message_timestamp, expense, prise, note) 
-                    VALUES (?, ?, ?, ?, ?, ?);""",
-                (user_id, message_id, message_timestamp, None, prise, note)
+                u"""
+                  INSERT INTO finance 
+                  (user_id,
+                  message_id,
+                  message_timestamp,
+                  expense,
+                  prise,
+                  note)
+                  VALUES (?, ?, ?, ?, ?, ?);
+                    """,
+                (user_id, message_id, message_timestamp, expense, prise, note)
             )
 
-    def delete_expense_by_timestamp(self, timestamp):
+    def delete_expense_by_message_id(self, message_id, text=None):
         with self.connection:
-            self.cursor.execute('DELETE FROM finance WHERE message_timestamp=? AND expense IS NULL;', (timestamp,))
+            if not text:
+                self.cursor.execute(
+                    u"""DELETE FROM finance WHERE message_id=?;""", (message_id,)
+                )
+            else:
+                self.cursor.execute(
+                    u"""DELETE FROM finance WHERE message_id=? and note=?;""", (message_id, text)
+                )
 
-    def delete_expense_by_id(self, id):
-        with self.connection:
-            self.cursor.execute('DELETE FROM finance WHERE message_id=?;', (id,))
-
-    def find_prior_expense(self, message_timestamp):
+    def find_expense_by_timestamp(self, message_timestamp):
         with self.connection:
             return self.cursor.execute(
-                'SELECT expense from finance WHERE message_timestamp=? AND expense IS NOT NULL LIMIT 1;', (message_timestamp,)
-            ).fetchall()
+                u"""SELECT expense from finance WHERE message_timestamp=? AND expense IS NOT NULL LIMIT 1;""",
+                (message_timestamp,)
+            ).fetchone()
 
     def find_expense(self, note):
         with self.connection:
             return self.cursor.execute(
-                'SELECT DISTINCT expense from finance WHERE note=? AND expense IS NOT NULL;', (note,)
-            ).fetchall()
+                u"""SELECT DISTINCT expense from finance WHERE note=? AND expense IS NOT NULL;""",
+                (note,)
+            ).fetchone()
 
-    def update_expense(self, message_id, expense):
-        with self.connection:
-            self.cursor.execute('UPDATE finance SET expense=? WHERE message_id=?;', (expense, message_id))
+    def update_expense(self, message_id, expense, text=None):
+        if not text:
+            with self.connection:
+                self.cursor.execute(
+                    u"""UPDATE finance SET expense=? WHERE message_id=?;""",
+                    (expense, message_id)
+                )
+        else:
+            with self.connection:
+                self.cursor.execute(
+                    u"""UPDATE finance SET expense=? WHERE message_id=? and note=?;""",
+                    (expense, message_id, text)
+                )
 
     def stat_by_total_month(self, month_ts):
         with self.connection:
             return self.cursor.execute(
-                'SELECT SUM(prise) from finance WHERE message_timestamp > ?;', (month_ts,)
-            ).fetchall()[0][0]
-
-    def close(self):
-        """ Закрываем текущее соединение с БД """
-        self.connection.close()
+                u"""SELECT SUM(prise) from finance WHERE message_timestamp > ?;""",
+                (month_ts,)
+            ).fetchone()
